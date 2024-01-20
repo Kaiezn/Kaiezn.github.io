5018b271492a4b0ee3f1f70bc200192ae79b1892
Kaiezn <108694767+Kaiezn@users.noreply.github.com>
2024-01-20 11:27:46 +0800

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
+
